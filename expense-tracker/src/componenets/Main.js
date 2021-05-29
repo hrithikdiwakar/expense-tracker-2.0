78d9a475c157c36df5ba25ccdc79a79a28e414f9
hrithikdiwakar13@gmail.com <hrithikdiwakar13@gmail.com>
2021-05-29 17:49:19 +0530

    init

---
+++ expense-tracker/src/componenets/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Main.css'
+class Main extends React.Component{
+    render(){
+        return(
+            <div>
+                I am the main component
+            </div>
+        );
+    }
+}
+
+export default Main;
