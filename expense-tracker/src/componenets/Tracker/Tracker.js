b61c594b903693683a6f5d0c72d68560a2b1971e
hrithikdiwakar13@gmail.com <hrithikdiwakar13@gmail.com>
2021-05-31 12:31:53 +0530

    added tracker file

--- expense-tracker/src/componenets/Tracker/Tracker.js
+++ expense-tracker/src/componenets/Tracker/Tracker.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import fire from '../../config/fire.js'
+import './Tracker.css'
+
 
 
 class  Tracker extends React.Component {
@@ -10,9 +12,63 @@ class  Tracker extends React.Component {
     render(){
 
     return (
-        <div>
-            Tracker
-            <button onClick={this.logout}> Logout</button>
+        <div className='container'>
+            <div className=''>
+                <h3> Expense Tracker </h3>
+                <h4>Balance</h4>
+                <h1>$0.00</h1>
+            </div>
+
+            <div className='income-expense'>
+                <div >
+                    <h5>INCOME</h5>
+                    <p id='money-plus' className='money plus'> +$0.00</p>
+                </div>
+                <div >
+                    <h5>EXPENSE</h5>
+                    <p id='money-minus' className='money minus'> -$0.00</p>
+                </div>
+            </div>
+
+            <div>
+                <h4>History</h4>
+                <hr />
+                <ul className='list'>
+                    <li className='plus'>Salary <span>+10000000</span></li>
+                    <li className='minus'>Movie Ticket <span>-100</span></li>
+                </ul>
+            </div>
+
+            <div>
+                <h4>Add new transaction</h4>
+                <hr />
+                <div className="transaction-text">
+                    <h5>Text</h5>
+                    <input type='text' placeholer="Enter Text...." />
+                </div>
+                <div className="new-transaction">
+                    <div>
+                        <select name="type">
+                            <option value="0">Type</option>
+                            <option value="deposit">Deposit</option>
+                            <option value="expense">Expense</option>
+                        </select>
+                    </div>
+                    <div>
+                        <input type="text" placeholder="Price" name="price" />
+                    </div>
+                </div>
+            </div>
+
+
+
+            
+
+            
+            
+            
+            
+            {/* <button onClick={this.logout}> Logout</button> */}
         </div>
     )
     }
